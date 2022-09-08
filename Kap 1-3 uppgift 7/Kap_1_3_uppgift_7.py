e2bc388b6c750795cdbf4ad41ad4751610ff86dd
felix <felix@DESKTOP-SSJR4EI>
2022-09-08 21:10:18 +0200

    Add project files.

---
+++ Kap 1-3 uppgift 7/Kap_1_3_uppgift_7.py
@@ -0,0 +1,10 @@
+tal1=float(input("Tal 1. ="))
+tal2=float(input("Tal 2. ="))
+tal3=float(input("Tal 3. ="))
+summa=tal1+tal2+tal3
+
+print("  ",'%.2f'%tal1)
+print("  ",'%.2f'%tal2)
+print("+ ",'%.2f'%tal3)
+print("========")
+print(" ",'%.2f'%summa)
